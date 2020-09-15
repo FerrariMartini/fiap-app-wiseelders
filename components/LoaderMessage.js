311d6d5d2fc8360567a5a67e6591dfc8930bc2c6
Rodolfo Penha <rodolfo.penha@gmail.com>
2020-09-14 23:00:58 -0300

    Formatacao

--- components/LoaderMessage.js
+++ components/LoaderMessage.js
@@ -13,11 +13,11 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
     },
     textPanel: {
-        color: theme.colors.primary
-    }
+        color: theme.colors.primary,
+    },
 });
 
 export default LoaderMessage;
